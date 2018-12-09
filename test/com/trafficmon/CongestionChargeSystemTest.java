7241a01ddcd86f6ccadd7a92d5e38d10225b3c22
yunpcy <32332492+yunpcy@users.noreply.github.com>
2018-12-09 23:23:41 +0000

    yeah

--- test/com/trafficmon/CongestionChargeSystemTest.java
+++ test/com/trafficmon/CongestionChargeSystemTest.java
@@ -25,6 +25,8 @@ public class CongestionChargeSystemTest {
     private final Vehicle vehicle1 = Vehicle.withRegistration("A123 XYZ");
     private final Vehicle vehicle2 = Vehicle.withRegistration("J091 4PY");
 
+    private Vehicle vehicle;
+
 
 
 
@@ -91,7 +93,7 @@ public class CongestionChargeSystemTest {
 
     }
 
-    
+
     @Test
     /**
      * Will issue penalty notice for disordering events
@@ -118,6 +120,90 @@ public class CongestionChargeSystemTest {
 
         Assert.assertTrue(vehiclesRecord.previouslyRegistered(vehicle1, eventLog));
         Assert.assertFalse(vehiclesRecord.previouslyRegistered(vehicle2, eventLog));
+    }
+
+
+    @Test
+    /**
+     check
+     if the exit time before the entry time
+     then it should return false
+     */
+    public void testCrossingBeforeLastEventError(){
+        List<ZoneBoundaryCrossing> crossings = new ArrayList<ZoneBoundaryCrossing>();
+        EntryEvent entry = new EntryEvent(Vehicle.withRegistration("A123 XYZ"));
+        ExitEvent exit = new ExitEvent(Vehicle.withRegistration("A123 XYZ"));
+        VehiclesRecord checkOrdering = new VehiclesRecord();
+
+        crossings.add(entry);
+        crossings.add(exit);
+
+        ZoneBoundaryCrossing lastEvent = crossings.get(0);
+        ZoneBoundaryCrossing crossing = crossings.get(1);
+        lastEvent.setTimestamp(10000);
+        crossing.setTimestamp(5000);
+        assertFalse(checkOrdering.checkOrderingOf(crossings,vehicle));
+    }
+
+
+    @Test
+    /**
+     check
+     if an entry is followed by an entry
+     then it should return false
+     */
+    public void testEntryBeforeEntryError(){
+        List<ZoneBoundaryCrossing> crossings = new ArrayList<ZoneBoundaryCrossing>();
+        EntryEvent entry1 = new EntryEvent(Vehicle.withRegistration("A123 XYZ"));
+        EntryEvent entry2 = new EntryEvent(Vehicle.withRegistration("A123 XYZ"));
+        VehiclesRecord checkOrdering = new VehiclesRecord();
+
+        crossings.add(entry1);
+        crossings.add(entry2);
+
+        assertFalse(checkOrdering.checkOrderingOf(crossings,vehicle));
+    }
+
+
+    @Test
+    /**
+     check
+     if an exit is followed by an exit
+     then is should return false
+     */
+    public void testExitBeforeExitError(){
+        List<ZoneBoundaryCrossing> crossings = new ArrayList<ZoneBoundaryCrossing>();
+        ExitEvent exit1 = new ExitEvent(Vehicle.withRegistration("A123 XYZ"));
+        ExitEvent exit2 = new ExitEvent(Vehicle.withRegistration("A123 XYZ"));
+        VehiclesRecord checkOrdering = new VehiclesRecord();
+
+        crossings.add(exit1);
+        crossings.add(exit2);
+
+        assertFalse(checkOrdering.checkOrderingOf(crossings,vehicle));
+    }
+
+
+    @Test
+    /**
+     check
+     if an entry is followed by an exit
+     then it should return true
+     */
+    public void testCrossingAfterLastEvent(){
+        List<ZoneBoundaryCrossing> crossings = new ArrayList<ZoneBoundaryCrossing>();
+        EntryEvent entry = new EntryEvent(Vehicle.withRegistration("A123 XYZ"));
+        ExitEvent exit = new ExitEvent(Vehicle.withRegistration("A123 XYZ"));
+        VehiclesRecord checkOrdering = new VehiclesRecord();
+
+        crossings.add(entry);
+        crossings.add(exit);
+
+        ZoneBoundaryCrossing lastEvent = crossings.get(0);
+        ZoneBoundaryCrossing crossing = crossings.get(1);
+        lastEvent.setTimestamp(5000);
+        crossing.setTimestamp(10000);
+        assertTrue(checkOrdering.checkOrderingOf(crossings,vehicle));
     }
 
 }
